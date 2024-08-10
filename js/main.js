3c9c4e9498b29ade8c28f0a5f3b638a91d220b9d
Hoang thang <lion1912001@gmail.com>
2024-08-10 23:49:09 +0700

    cm
    home, chủ đề, chi tiết chủ đề

--- js/main.js
+++ js/main.js
@@ -54,6 +54,42 @@
                 },
             ]
         });
+        jQuery('.favorites-slider-2').slick({
+            dots:false,
+            arrow : true,
+            infinite : true,
+            speed : 300,
+            autoplay : false,
+            slidesToShow : 6,
+            slidesToScroll :1,
+            nextArrow: '<a href="#" class="slick-arrow slick-next position-absolute text-decoration-none"><img src="./imgs/arrow-right-2.svg" alt=""></a>',
+            prevArrow: '<a href="#" class="slick-arrow slick-prev position-absolute text-decoration-none d-none"><img src="./imgs/arrow-left.svg" alt=""></a>',
+            responsive : [
+                {
+                    breakpoint:1200,
+                    settings : {
+                        slidesToShow : 3,
+                        slidesToScroll : 1,
+                        infinite : true,
+                        dots : true
+                    }
+                },
+                {
+                    breakpoint:768,
+                    settings : {
+                        slidesToShow : 2,
+                        slidesToScroll : 1
+                    }
+                },
+                {
+                    breakpoint:480,
+                    settings : {
+                        slidesToShow : 1,
+                        slidesToScroll : 1
+                    }
+                },
+            ]
+        });
     });
 })(jQuery);
 
